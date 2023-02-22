d0eae746b532e95dd6945d6fa33e04f959c3165b
laetitia-piat <63997891+laetitia-piat@users.noreply.github.com>
2023-02-22 14:09:38 +0100

    Update calcul.js

--- calcul.js
+++ calcul.js
@@ -2,12 +2,12 @@ let nombreHeureTotale = prompt("Combien d'heures travaillées?");
 let nombreHeureDimanche = prompt("Dont combien d'heure de dimanche?");
 
 
-let salaireDeBaseMensualise =  11.07 * 151.67; 
-let absencePourEntreeSortie = (151.67-(nombreHeureTotale)) * 12.6392;
+let salaireDeBaseMensualise =  11.27 * 151.67; 
+let absencePourEntreeSortie = (151.67-(nombreHeureTotale)) * 12.8400;
 let salaireBase = salaireDeBaseMensualise - absencePourEntreeSortie;
 let indemniteSujetionSpeciale =(nombreHeureTotale*10.80545)*0.0921 ;
 let revalorisationSegur = 238;
-let majoDimanche = nombreHeureDimanche * 7.64; 
+let majoDimanche = nombreHeureDimanche * 7.86; 
 //FIN DE CONTRAT
 let indemnitePrecarite = (salaireBase+indemniteSujetionSpeciale+revalorisationSegur+majoDimanche)*10/100;
 console.log (indemnitePrecarite);
