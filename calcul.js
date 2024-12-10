46bcc767365d5689e1b928120e8523bf8f509ec7
laetitia-piat <63997891+laetitia-piat@users.noreply.github.com>
2024-12-10 10:41:25 +0100

    Update calcul.js

--- calcul.js
+++ calcul.js
@@ -2,28 +2,27 @@
 document.getElementById("calcul").addEventListener('click', function() {
 let 
     nombreHeureTotale = document.getElementById("heureTotale").value,
-    console.log("nombreHeureTotale", nombreHeureTotale),
     nombreHeureDimanche = document.getElementById("heureDimanche").value,
-    console.log("nombreHeureDimanche", nombreHeureDimanche),
     salaireDeBaseMensualise =  11.65 * 151.67,
-    console.log("salaireDeBaseMensualise", salaireDeBaseMensualise)
     absencePourEntreeSortie = (151.67-(nombreHeureTotale)) * 13.2192,
-    console.log("absencePourEntreeSortie", absencePourEntreeSortie)
     salaireBase = salaireDeBaseMensualise - absencePourEntreeSortie,
-        console.log("salaireBase", salaireBase)
-    indemniteSujetionSpeciale =(nombreHeureTotale*11.11)*0.0921,
-        console.log("indemniteSujetionSpeciale", indemniteSujetionSpeciale)
+    indemniteSujetionSpeciale =(nombreHeureTotale*11.11)*0.0921,  
     majoDimanche = nombreHeureDimanche * 9.77,
-        console.log("majoDimanche", majoDimanche)
     indemnitePrecarite = (salaireBase+indemniteSujetionSpeciale+238+majoDimanche)*10/100,
-        console.log("indemnitePrecarite", indemnitePrecarite)
-    indemniteCongesPayes = (salaireBase+indemniteSujetionSpeciale+238+majoDimanche+indemnitePrecarite) *10/100,
-        console.log("indemniteCongesPayes", indemniteCongesPayes)
+    indemniteCongesPayes = (salaireBase+indemniteSujetionSpeciale+238+majoDimanche+indemnitePrecarite) *10/100, 
     salaireBrut = salaireBase + 238 + indemniteSujetionSpeciale + majoDimanche + indemnitePrecarite + indemniteCongesPayes,
-        console.log("salaireBrut", salaireBrut)
-    
     salaireNet = document.querySelector('input[name="valeur_data"]');
 
 salaireNet.value = Math.round((salaireBrut/ 1.2844)*100)/100;
     });  
+  console.log("nombreHeureTotale", nombreHeureTotale),
+console.log("nombreHeureDimanche", nombreHeureDimanche),
+console.log("salaireDeBaseMensualise", salaireDeBaseMensualise),
+ console.log("absencePourEntreeSortie", absencePourEntreeSortie),
+       console.log("salaireBase", salaireBase),
+console.log("indemniteSujetionSpeciale", indemniteSujetionSpeciale),
+ console.log("majoDimanche", majoDimanche)
+console.log("indemnitePrecarite", indemnitePrecarite)
+  console.log("indemniteCongesPayes", indemniteCongesPayes)    
+     console.log("salaireBrut", salaireBrut)  
 
